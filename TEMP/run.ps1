6cf424bdf1b804f9fe4e42d7702d7b8b9b3e124a
czyzadam <czyz.adam@gmail.com>
2024-06-15 15:41:57 +0200

    Added ARM templates

---
+++ TEMP/run.ps1
@@ -0,0 +1,12 @@
+#$templateFile = "C:\4.Projects\CMF_Devops\azuredeploy.json"
+#$parameterFile="C:\4.Projects\CMF_Devops\azuredeploy.parameters.ada.json"
+
+New-AzResourceGroup `
+  -Name Azure-CMF-core-network `
+  -Location "Poland Central"
+  
+#New-AzResourceGroupDeployment `
+# -Name devenvironment `
+#  -ResourceGroupName Azure-CMF-core-network `
+#  -TemplateFile $templateFile `
+#  -TemplateParameterFile $parameterFile
